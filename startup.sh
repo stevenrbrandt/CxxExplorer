b91cdbb0e64cd8c2f5ba0907cff4fe37ee66f72d
Steven R. Brandt <sbrandt@cct.lsu.edu>
2020-08-25 13:14:19 -0500

    Create old users on startup

--- startup.sh
+++ startup.sh
@@ -8,5 +8,15 @@ then
     source /oauth/oauth.sh
 fi
 touch /usr/enable_mkuser
+
+# Re-create all existing users with the correct id
+for h in /home/*
+do
+  if [ -r $h/.bashrc ]
+  then
+     mkuser $(basename $h)
+  fi
+done
+
 echo jupyterhub --ip 0.0.0.0 --port $PORT -f jup-config.py
 jupyterhub --ip 0.0.0.0 --port $PORT -f jup-config.py
