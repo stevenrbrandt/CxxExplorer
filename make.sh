3c9b90dea1ae088b4075e8fc16c931b387bf5770
Steven R. Brandt <sbrandt@cct.lsu.edu>
2023-09-13 17:15:02 +0000

    Add 20

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 export NCPUS=$(lscpu|grep '^CPU.s.:'|cut -d: -f2)
-export BUILD_CPUS=$(($NCPUS/2))
+export BUILD_CPUS=2 #$(($NCPUS/2))
 make VERBOSE=yes -j $BUILD_CPUS install 2>&1 | tee make.out
 cd /usr/install/cling
 find . -type f -a \( -name \*.o -o -name \*.cmake -o -name \*.cpp -o -name \*.h -name \*.c \) | xargs rm -f
+which cling
